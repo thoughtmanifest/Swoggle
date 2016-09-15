3cea1fb2775c2f68d20e5241c69993228bdb64d7
David Sison <daviddsison@gmail.com>
2016-09-15 13:42:40 -0400

    Initial Commit

---
+++ Swoggle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swoggle
+//
+//  Created by David Sison on 9/15/16.
+//  Copyright © 2016 David Sison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
